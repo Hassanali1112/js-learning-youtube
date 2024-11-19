da42d238e1d221355b72b4776967c0ce2f5a4113
hassan ali <hassanali0111272@gmail.com>
2024-11-19 22:43:18 +0500

    variable and constant

---
+++ variables.js
@@ -0,0 +1,15 @@
+const accountId = 123445;
+let accountEmail = "hassanali004@gmail.com";
+var accountPassword = "hassan0312.";
+accountCity = "karachi";
+let accountState;
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+initially javascript does have control over scope if value
+ of a varible is change inside any code block it will change 
+globaly to get rid of that let keyword was intreduced 
+Prefer not to use var use let
+*/
