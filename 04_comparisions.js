6bf158a96b2ae831cccf3d2dee5da04cc53233a4
hassan ali <hassanali0111272@gmail.com>
2024-11-22 21:01:14 +0500

    comparision practice done

---
+++ 04_comparisions.js
@@ -0,0 +1,24 @@
+// comparion of same datatypes
+
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+// comparision of different datatypes
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+console.log( null > 0 );
+console.log( null < 0 );
+console.log( null >= 0 );
+console.log( null <= 0 );
+
+console.log( undefined > 0 );
+console.log( undefined < 0 );
+console.log( undefined >= 0 );
+console.log( undefined <= 0 )
+
+// their output unpridictable so avoid using then
