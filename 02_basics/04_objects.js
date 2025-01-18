b0f7e1396dee96348fb3b7880b0b5f689c41de54
hassan ali <hassanali0111272@gmail.com>
2025-01-18 15:39:58 +0500

    object practice 2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,25 @@
+// const tinderUser = new Object();
+
+const tinderUser = {};
+tinderUser.name = "hassan ali";
+tinderUser.email = "hassan@mail.com";
+
+// console.log(tinderUser)
+
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "c", 4: "d" };
+const obj3 = { 5: "e", 6: "f" };
+
+// const obj4 = Object.assign( obj1, obj2, obj3)
+// const obj4 = Object.assign({}, obj1, obj2, obj3) // best practice
+
+const obj4 = { ...obj1, ...obj2, ...obj3 };
+
+// console.log(obj4);
+
+// console.log(Object.keys(obj4));
+// console.log(Object.values(obj4));
+// console.log(Object.entries(obj4));
+
+console.log(tinderUser.hasOwnProperty("age"));
+console.log(tinderUser.hasOwnProperty("email"));
