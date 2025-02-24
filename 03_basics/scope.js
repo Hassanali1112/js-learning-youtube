bb86913e02200ce0b9c2190603653ee070984a54
hassan ali <hassanali0111272@gmail.com>
2025-02-24 19:16:09 +0500

    scope part 02 done

--- 03_basics/scope.js
+++ 03_basics/scope.js
@@ -7,8 +7,56 @@ const c = 600;
   let b = 40;
   const c = 60;
 
-  console.log(" at line 10 a : ", a, " b : ", b, " c : ", c);
+  // console.log(" at line 10 a : ", a, " b : ", b, " c : ", c);
 
 }
 
-console.log("at line 14 a : " , a , " b : " , b , " c : " , c);
+// console.log("at line 14 a : " , a , " b : " , b , " c : " , c);
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+function one(){
+  const studentName = "Hassan Ali ";
+
+  function two (){
+    const trainer = "Hitesh"
+    const source = "Youtube"
+
+    console.log(studentName  + trainer)
+  }
+  // console.log(source)
+  two()
+
+}
+
+// one()
+
+const studentName = "Hassan Ali "
+
+if(studentName){
+  const trainerName = "Hitesh";
+  if(trainerName){
+    const source = "Youtube.com";
+    console.log(`Name ${studentName}, trainer ${trainerName} and source is ${source}`)
+  }
+
+  // console.log(source)
+}
+// console.log(trainerName)
+
+// +++++++++++++++++++++++++++++++++++++++++++++ interesting +++++++++++++++++++++++++++++++++++++++++++
+
+console.log(addOne(5))
+function addOne (num){
+  return num + 1;
+}
+
+// function Expression
+
+// console.log(addTwo(5))
+const addTwo = function(num){
+  return num + 2;
+}
+
+// this can't be access before initialization
