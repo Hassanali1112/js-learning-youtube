c7b1adfa5c52ba4197c4c07f3d4b5cc36135e056
hassan ali <hassanali0111272@gmail.com>
2025-02-27 22:45:46 +0500

    control flow switch done

---
+++ 05_control-flow/switch.js
@@ -0,0 +1,43 @@
+const month = "march";
+
+switch (month) {
+  case "jan":
+    console.log(month)
+    break;
+
+  case "fab":
+    console.log(month)
+    break;
+
+  case "march":
+    console.log(month)
+    break;
+  case "april":
+    console.log(month)
+    break;
+
+  default:
+    break;
+}
+
+const numMonth = 4;
+
+switch (numMonth) {
+  case 1:
+    console.log(numMonth);
+    break;
+
+  case 2:
+    console.log(numMonth);
+    break;
+
+  case 3:
+    console.log(numMonth);
+    break;
+  case 4:
+    console.log(numMonth);
+    break;
+
+  default:
+    break;
+}
