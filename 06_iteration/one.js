925396ae5a7a154cd54efe668aedaea3418dbca3
hassan ali <hassanali0111272@gmail.com>
2025-02-28 07:12:30 +0500

    for loop done

---
+++ 06_iteration/one.js
@@ -0,0 +1,30 @@
+for (let index = 0; index < 10; index++) {
+  // console.log(index)
+  
+}
+
+for(let j = 0; j < 8; j++){ 
+  if(j === 6){
+    // break
+    continue
+    console.log(`matched Case`+j)
+  }
+  console.log(j);
+
+}
+
+for(let i =2; i <= 10;  i++) {
+    for(j=1; j <= 10; j++){
+      console.log(`${i} X ${j} = ${i * j}`)
+    }
+}
+
+const arr = ["Taimoor","Hassan Ali","Abdul Rehman","Zeeshan","Faizan"]
+
+for(let i = 0; i < arr.length; i++){
+  if(arr[i]=== "Hassan Ali"){
+    console.log(`welcome ${arr[i]}`)
+    continue
+  }
+  console.log(`brothers ${arr[i]}`)
+}
