4a8a597576cb4d13c6c35ff8c32c4d080af5ac48
hassan ali <hassanali0111272@gmail.com>
2025-03-26 21:36:01 +0500

    done with recommended approach

--- 07_projects/project_06(change background every second)/changeBackground.js
+++ 07_projects/project_06(change background every second)/changeBackground.js
@@ -2,21 +2,38 @@ const startButton = document.querySelector("#start");
 const stopButton = document.querySelector("#stop");
 const body = document.querySelector("body");
 
-const generateHexCode = () =>{
-    let hex = Math.round(Math.random() * 256 + 1);
-    return hex
+// _____________________________________________  recommended approach __________________________________________________
 
-}
+const generateHexCode = () => {
+  const hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
 
-let  bgInterval;
-  startButton.addEventListener("click",()=>{
-    bgInterval = setInterval(() => {
-      body.style.backgroundColor = `rgb(${generateHexCode()},${generateHexCode()},${generateHexCode()})`;
-    }, 1000);
-  },false)
+console.log(generateHexCode());
 
-  stopButton.addEventListener("click",()=>{
-    if(bgInterval){
+let bgInterval;
+startButton.addEventListener(
+  "click",
+  () => {
+    if (!bgInterval) {
+      console.log(bgInterval)
+      bgInterval = setInterval(() => {
+        body.style.backgroundColor = `${generateHexCode()}`;
+      }, 1000);
+    }
+    
+  },
+  false
+);
+
+stopButton.addEventListener(
+  "click",
+  () => {
+    if (bgInterval) {
       Swal.fire({
         title: "Are you sure to stop it?",
         icon: "warning",
@@ -26,8 +43,9 @@ let  bgInterval;
         confirmButtonText: "Yes, stop it!",
       }).then((result) => {
         if (result.isConfirmed) {
-      clearInterval(bgInterval);
-      body.style.backgroundColor = `black`;
+          clearInterval(bgInterval);
+          body.style.backgroundColor = `black`;
+          bgInterval = null;
           Swal.fire({
             title: "stopped",
             text: "background change has been stopped",
@@ -35,11 +53,62 @@ let  bgInterval;
           });
         }
       });
-    } else{
+    } else {
       Swal.fire({
-        title: "No Background?",
-        text: "kindly press start button first?",
+        title: "No Background found",
+        text: "Do you want to start it?",
         icon: "error",
+        confirmButtonColor: "green",
+        confirmButtonText: "Yes, start it!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          startButton.click();
+        }
       });
     }
-  },false)
+  },
+  false
+);
+
+// _________________________________________________________ my approach __________________________________________________
+// const generateHexCode = () =>{
+//     let hex = Math.round(Math.random() * 256 + 1);
+//     return hex
+
+// }
+
+// let  bgInterval;
+//   startButton.addEventListener("click",()=>{
+//     bgInterval = setInterval(() => {
+//       body.style.backgroundColor = `rgb(${generateHexCode()},${generateHexCode()},${generateHexCode()})`;
+//     }, 1000);
+//   },false)
+
+//   stopButton.addEventListener("click",()=>{
+//     if(bgInterval){
+//       Swal.fire({
+//         title: "Are you sure to stop it?",
+//         icon: "warning",
+//         showCancelButton: true,
+//         confirmButtonColor: "#3085d6",
+//         cancelButtonColor: "#d33",
+//         confirmButtonText: "Yes, stop it!",
+//       }).then((result) => {
+//         if (result.isConfirmed) {
+//       clearInterval(bgInterval);
+//       body.style.backgroundColor = `black`;
+//           Swal.fire({
+//             title: "stopped",
+//             text: "background change has been stopped",
+//             icon: "success",
+//           });
+//         }
+//       });
+//     } else{
+//       Swal.fire({
+//         title: "No Background?",
+//         text: "kindly press start button first?",
+//         icon: "error",
+//       });
+//     }
+//   },false)
